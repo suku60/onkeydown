09387c2d93ce5951369c6ba311b1538a75ab8d70
Juanma Stella <0x86jm14sMb94@gmail.com>
2022-10-15 13:54:32 +0200

    basic style done.

--- index.js
+++ index.js
@@ -3,10 +3,24 @@ const init = () => {
 }
 
 const keydown = (e) => {
-    let indicator = document.getElementById("indicator") ;
 
-    indicator.textContent = e.keyCode
-    
+    let indicator = document.getElementById("indicator") ;    
+
+    let size = Math.floor(Math.random() * (2.5 - 1.8 + 1) + 1.8)
+
+    indicator.textContent = e.keyCode;
+    indicator.style.fontSize = size + "em";
+
+    if(indicator.style.color === "black"){
+    indicator.style.fontSize = size + "em";
+    indicator.style.color = "#B3A49D";
+    }else{
+        indicator.style.color = "black";
+        indicator.style.fontSize = "2em"
+
+
+    }
+
     console.log(e.keyCode)
 }
 
