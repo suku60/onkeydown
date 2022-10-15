a4c095e9cf0210981e0e993872f8c3348b83c7ba
Juanma Stella <0x86jm14sMb94@gmail.com>
2022-10-15 14:05:59 +0200

    added final colors.

--- main.js
+++ main.js
@@ -8,17 +8,17 @@ const keydown = (e) => {
 
     let size = Math.floor(Math.random() * (2.5 - 1.8 + 1) + 1.8)
 
-    indicator.textContent = e.keyCode;
-    indicator.style.fontSize = size + "em";
+    indicator.textContent =  e.keyCode;
 
     if(indicator.style.color === "black"){
-    indicator.style.fontSize = size + "em";
-    indicator.style.color = "#B3A49D";
+        indicator.style.fontSize = size + "em";
+        indicator.style.opacity = "1";
+        indicator.style.color = "#FFA363";
     }else{
+        indicator.style.fontSize = size + "em";
         indicator.style.color = "black";
+        indicator.style.opacity = ".8";
         indicator.style.fontSize = "2em"
-
-
     }
 
     console.log(e.keyCode)
