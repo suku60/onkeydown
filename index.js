c9aca8b34a9ac39f25142532d2befbe3bb788b92
Juanma Stella <0x86jm14sMb94@gmail.com>
2022-10-15 12:40:25 +0200

    painted keyCode succesfully.

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const init = () => {
-    
     document.addEventListener("keydown", keydown)
 }
 
 const keydown = (e) => {
+    let indicator = document.getElementById("indicator") ;
+
+    indicator.textContent = e.keyCode
+    
     console.log(e.keyCode)
 }
 
